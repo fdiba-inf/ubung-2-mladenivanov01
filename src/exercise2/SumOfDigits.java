f00eb0d1b48564b20304bc0c053234258f1eea57
mladenivanov01 <mladenivanov01@gmail.com>
2020-10-07 06:13:58 +0000

    .

---
+++ src/exercise2/SumOfDigits.java
@@ -0,0 +1,14 @@
+package exercise2;
+
+import java.util.Scanner;
+
+
+public class SumOfDigits {
+ public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+  }
+}
